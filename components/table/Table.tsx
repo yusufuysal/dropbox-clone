96330d605376b74c5a5a377b792cbbb9ce1244e4
yusufuysal <yusuf.uysal@agu.edu.tr>
2024-06-05 17:36:14 +0300

    Customize the table column views and icons

--- components/table/Table.tsx
+++ components/table/Table.tsx
@@ -15,6 +15,9 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { FileType } from "@/types";
+import { PencilIcon, TrashIcon } from "lucide-react";
+import { Button } from "../ui/button";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -61,9 +64,41 @@ export function DataTable<TData, TValue>({
               >
                 {row.getVisibleCells().map((cell) => (
                   <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    {cell.column.id === "timestamp" ? (
+                      <div className="flex flex-col">
+                        <div className="text-sm">
+                          {(cell.getValue() as Date).toLocaleDateString()}
+                        </div>
+                        <div className="text-xs text-gray-500">
+                          {(cell.getValue() as Date).toLocaleTimeString()}
+                        </div>
+                      </div>
+                    ) : cell.column.id === "filename" ? (
+                      <p
+                        onClick={() => console.log("open rename modal func")}
+                        className="underline flex items-center text-blue-500 hover:cursor-pointer"
+                      >
+                        {cell.getValue() as string}
+                        {""}
+                        <PencilIcon size={15} className="ml-2" />
+                      </p>
+                    ) : (
+                      flexRender(cell.column.columnDef.cell, cell.getContext())
+                    )}
                   </TableCell>
                 ))}
+
+                <TableCell key={(row.original as FileType).id}>
+                  <Button
+                    variant={"outline"}
+                    onClick={
+                      () => console.log("helllooo")
+                      //openDeleteModal((row.original as FileType).id)
+                    }
+                  >
+                    <TrashIcon size={20} />
+                  </Button>
+                </TableCell>
               </TableRow>
             ))
           ) : (
--- components/table/TableWrapper.tsx
+++ components/table/TableWrapper.tsx
@@ -60,6 +60,13 @@ function TableWrapper({ skeletonFiles }: { skeletonFiles: FileType[] }) {
               <Skeleton className="h-12 w-full" />
             </div>
           ))}
+
+          {skeletonFiles.length === 0 && (
+            <div className="flex items-center space-x-4 p-5 w-full">
+              <Skeleton className="h-12 w-12" />
+              <Skeleton className="h-12 w-full" />
+            </div>
+          )}
         </div>
       </div>
     );
